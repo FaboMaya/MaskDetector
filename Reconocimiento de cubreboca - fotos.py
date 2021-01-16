5aa4fdf452f02a67c9ea819b02be82228a5758a4
Fabián Maya Montero <67724139+FaboMaya@users.noreply.github.com>
2021-01-16 01:24:25 -0600

    Add files via upload

--- Reconocimiento de cubreboca - fotos.py
+++ Reconocimiento de cubreboca - fotos.py
@@ -3,38 +3,56 @@ import cv2 as cv
 import pickle
 import os
 
-carpeta = "Frames"
+carpeta = "Samples"
 nombresArchivos = os.listdir(carpeta)
-numeroArchivo = 4238
+numeroArchivo = 1 	# Usar cualquier número entre 0 y 19 para cambiar de imagen
 nombreArchivo = "{0}/{1}".format(carpeta,nombresArchivos[numeroArchivo])
 
+# Para cambiar el numero de etapas del clasificador en cascada al cambiar este número por cualquiera entre 1 y 5.
+cascada = 5 
+"""
+ 1 = 10 etapas
+ 2 = 20 etapas
+ 3 = 30 etapas
+ 4 = 40 etapas
+ 5 = 50 etapas
+"""
+
 # load the model from disk
-nombreArchivoClf = "modeloSVM.sav"
-clfSVM = pickle.load(open(nombreArchivoClf, 'rb'))
+nombreArchivoClf = "modeloMLP.sav"
+clfMLP = pickle.load(open(nombreArchivoClf, 'rb'))
 dataP = []
 
-for i in range(1,6):
-	cascade_face = cv.CascadeClassifier('cascade{}/cascade.xml'.format(i))
-	img = cv.imread(nombreArchivo)
-	rectangles = cascade_face.detectMultiScale(img)
-	if len(rectangles) > 0:
-		for rect in rectangles:
-			img = cv.rectangle(img,(rect[0],rect[1]),(rect[0]+rect[2],rect[1]+rect[3]),(255,0,0))
-			if i == 5:
-				face = np.array(img[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]])
-				face = cv.resize(img,(16,16))
-				face = cv.cvtColor(face,cv.COLOR_BGR2GRAY)
-				face = face//16
-				face = face.ravel()
-				print(face.shape)
-				dataP.append(face)
-	cv.imshow('img{}'.format(i), img)
+cascade_face = cv.CascadeClassifier('cascade{}/cascade.xml'.format(cascada))
+img = cv.imread(nombreArchivo)
+rectangles = cascade_face.detectMultiScale(img)
+	
+if len(rectangles) > 0:
+	for rect in rectangles:
+		face = np.array(img[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]])
+		face = cv.resize(face,(16,16))
+		face = cv.cvtColor(face,cv.COLOR_BGR2GRAY)
+		face = face//16
+		face = face.ravel()
+		#print(face.shape)
+		dataP.append(face)
+		predictedMLP = clfMLP.predict([face])
+		#print(5,"MLP:",predictedMLP)
+		color = None
+		if predictedMLP == ["Cubreboca"]:
+			color = (0,255,0)
+		elif predictedMLP == ["Otros"]:
+			color = (255,0,0)
+		else:
+			color = (0,0,255)
+		img = cv.rectangle(img,(rect[0],rect[1]),(rect[0]+rect[2],rect[1]+rect[3]),color,3)
+cv.imshow('img', img)
 
 dataP = np.array(dataP)
-print(len(dataP))
+#print(len(dataP))
 if len(dataP) > 0:
-	predictedSVM = clfSVM.predict(dataP[:])
-	print(5,"SVM:",predictedSVM)
+	predictedMLP = clfMLP.predict(dataP[:])
+	print(predictedMLP)
 else:
 	print("No se encontraron caras")
 
